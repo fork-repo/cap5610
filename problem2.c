ab6b2e83e9aeb226a540a8af7d102760f6e66bf6
Jian Zhou <i@janzhou.org>
2014-09-17 17:07:26 -0400

    Maximum-a-Posteriori (MAP) Estimation

--- problem2.c
+++ problem2.c
@@ -7,39 +7,42 @@
 #include "mnist.h"
 
 /* Maximum likelihood estimation
+ * Maximum A Posterior estimation
  */
 
-static void mle(void){
-  unsigned int digits[10], i;
+unsigned int digits[10];
+
+static void estimation(void){
+  unsigned int i;
+  float d;
 
   for(i = 0; i < 10; i++){
     digits[i] = 0;
   }
 
+  // # of digit d in training set
   for(i = 0; i < mnist_train; i++){
     digits[*(mnist_labels+i)]++;
   }
 
-  printf("Maximum likelihood estimation:\n");
-
-  for(i = 0; i < 10; i++){
-    printf("%d\t", i);
+  printf("digit\tcount\tmle\t");
+  for(d = 0.01; d <= 0.16; d=d*2) {
+    printf("map(%.0f%%)\t", 100*d); //map
   }
   printf("\n");
 
   for(i = 0; i < 10; i++){
-    printf("%d\t", digits[i]);
+    printf("%d\t%d\t", i,digits[i]); //digit count
+    printf("%.2f%%\t", ((float)digits[i] * 100)/mnist_train); //mle
+    for(d = 0.01; d <= 0.16; d=d*2) {
+      printf("%.2f%%\t", 100*((float)digits[i] + d*mnist_train)/(mnist_train + d*mnist_train*10)); //map
+    }
+    printf("\n");
   }
-  printf("\n");
-
-  for(i = 0; i < 10; i++){
-    printf("%.2f\%\t", ((float)digits[i] * 100)/mnist_train);
-  }
-  printf("\n");
 }
 
 int main(int argc, char ** argv) {
   load_idx();
-  mle();
+  estimation();
   return 0;
 }
