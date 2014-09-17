765f90acac49ccc533aaa34c6caa0c93d9fc41af
Jian Zhou <i@janzhou.org>
2014-09-17 16:05:11 -0400

    problem2 Maximum likelihood estimation

--- problem2.c
+++ problem2.c
@@ -6,10 +6,40 @@
 #include <pthread.h>
 #include "mnist.h"
 
-unsigned char *images;
-unsigned char *labels;
+/* Maximum likelihood estimation
+ */
+
+static void mle(void){
+  unsigned int digits[10], i;
+
+  for(i = 0; i < 10; i++){
+    digits[i] = 0;
+  }
+
+  for(i = 0; i < mnist_train; i++){
+    digits[*(mnist_labels+i)]++;
+  }
+
+  printf("Maximum likelihood estimation:\n");
+
+  for(i = 0; i < 10; i++){
+    printf("%d\t", i);
+  }
+  printf("\n");
+
+  for(i = 0; i < 10; i++){
+    printf("%d\t", digits[i]);
+  }
+  printf("\n");
+
+  for(i = 0; i < 10; i++){
+    printf("%.2f\%\t", ((float)digits[i] * 100)/mnist_train);
+  }
+  printf("\n");
+}
 
 int main(int argc, char ** argv) {
   load_idx();
+  mle();
   return 0;
 }
